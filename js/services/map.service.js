071c5e1e75916d472d2bcd74cdd37c11d7452d8a
Maya <mayac8771108@gmail.com>
2023-08-30 15:11:34 +0300

    add map

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -4,23 +4,48 @@ export const mapService = {
     panTo
 }
 
-
 // Var that is used throughout this Module (not global)
+window.initMap=initMap
 var gMap
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap')
     return _connectGoogleApi()
         .then(() => {
-            console.log('google available')
-            gMap = new google.maps.Map(
+            // console.log('google available')
+          return  gMap = new google.maps.Map(
                 document.querySelector('#map'), {
                 center: { lat, lng },
                 zoom: 15
             })
-            console.log('Map!', gMap)
+            // console.log('Map!', gMap)
         })
-}
+        .then (map=> {
+
+            let infoWindow = new google.maps.InfoWindow({
+                content: "Click the map to get Lat/Lng!",
+                position: map.center,
+              });
+            
+              infoWindow.open(map);
+              // Configure the click listener.
+              map.addListener("click", (mapsMouseEvent) => {
+                // Close the current InfoWindow.
+                infoWindow.close();
+                // Create a new InfoWindow.
+                infoWindow = new google.maps.InfoWindow({
+                  position: mapsMouseEvent.latLng,
+                });
+                infoWindow.setContent(
+                    JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2),
+                    );
+                    infoWindow.open(map);
+                });
+            }
+            
+            )
+            
+        }
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
@@ -39,7 +64,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyBn5ses1DT9LwwtDcpIAL8BO3CvHhkfZw0' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBn5ses1DT9LwwtDcpIAL8BO3CvHhkfZw0&callback=initMap' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
